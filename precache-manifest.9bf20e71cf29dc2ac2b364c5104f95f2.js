75b77a34cdca95f90e4106cce717c3809d4f6f61
Todd Leslie <spotontechnology@gmail.com>
2020-05-30 08:37:15 -0600

    Updates

---
+++ precache-manifest.9bf20e71cf29dc2ac2b364c5104f95f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018c5ea6e43cb15b986a57f040d37a92",
+    "url": "/reactPort/index.html"
+  },
+  {
+    "revision": "01bfa646a0a10b73d765",
+    "url": "/reactPort/static/css/main.8c58bbe4.chunk.css"
+  },
+  {
+    "revision": "8ee162effd3ebd3fc540",
+    "url": "/reactPort/static/js/2.3cbe636f.chunk.js"
+  },
+  {
+    "revision": "01bfa646a0a10b73d765",
+    "url": "/reactPort/static/js/main.3fa93762.chunk.js"
+  },
+  {
+    "revision": "d639e152f3bdee7964ca",
+    "url": "/reactPort/static/js/runtime~main.dbb28688.js"
+  },
+  {
+    "revision": "e2b646607ece8d99f48e4e21ddd5f887",
+    "url": "/reactPort/static/media/IMG_03891.e2b64660.jpg"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/reactPort/static/media/csslogo.04cf122b.png"
+  },
+  {
+    "revision": "719fefb4c89c646aedd5bb462af9d8ff",
+    "url": "/reactPort/static/media/database.719fefb4.jpg"
+  },
+  {
+    "revision": "3a431a93c33adf4f325cfbd1547c8149",
+    "url": "/reactPort/static/media/html5logo.3a431a93.jpg"
+  },
+  {
+    "revision": "184a7681dfc7ae09341edaafc979da8f",
+    "url": "/reactPort/static/media/javascriptimg2.184a7681.png"
+  },
+  {
+    "revision": "d261a13e651f8cdc82a378b29f155537",
+    "url": "/reactPort/static/media/mngodbimg.d261a13e.png"
+  }
+]);
